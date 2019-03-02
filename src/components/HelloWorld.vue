ab52f2e81d9b5cb46b5350c3d6f2daf31bd4d56d
e <954480192@qq.com>
2019-03-02 21:56:28 +0800

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <el-row>
+      <el-col :span="24"><div class="grid-content bg-purple-dark">sdffsfs</div></el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+</style>
